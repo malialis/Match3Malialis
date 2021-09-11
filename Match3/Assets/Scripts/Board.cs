4aa04d00fc160944e619ca7eab0d4cbb429df316
nick malialis <batnick@gmail.com>
2021-09-11 16:37:37 -0600

    ScriptsYo
    ScriptsYo

--- Match3/Assets/Scripts/Board.cs
+++ Match3/Assets/Scripts/Board.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    [SerializeField]private int width;
+    [SerializeField]private int height;
+
+    public GameObject bgTilePrefab;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Setup();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Setup()
     {
-        
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Vector2 pos = new Vector2(x, y);
+                GameObject bgTile = Instantiate(bgTilePrefab, pos, Quaternion.identity);
+                bgTile.transform.parent = transform;
+                bgTile.name = "BG Tile - " + x + ", " + y;
+            }
+        }
+
     }
+
+
+
+
+
 }
